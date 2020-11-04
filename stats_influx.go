8179e18b8a3f54a460100949a8dd5bd70e1107bc
Javier Bustos <jbustos@niclabs.cl>
2020-11-04 15:16:11 -0300

    separating by database

--- stats_influx.go
+++ stats_influx.go
@@ -11,13 +11,11 @@ import (
 
 
 /*
-
 From:
 Detecting Anomalies at a TLD Name Server Based on DNS Traffic Predictions
 Diego Madariaga, Javier Madariaga, Martı́n Panza, Javier Bustos-Jiménez, 
 and Benjamin Bustos
 IEEE Transactions on Network and Service Management (IEEE TNSM) Journal
-
 Given the aforementioned, our proposed AD-BoP method
 is focused on the following nine DNS traffic features:
 • Number of DNS queries of types A (1), AAAA (2), NS
@@ -28,58 +26,48 @@ is focused on the following nine DNS traffic features:
 • Total number of DNS packets (9).
 */
 
-func InfluxAggAndStore(writeAPI api.WriteAPI, batch []cdns.DNSResult) error {
+func aggAndStore(writeAPI api.WriteAPI, batch []cdns.DNSResult) error {
 	defer writeAPI.Flush()
 
-	type Q struct {
+	type Key struct {
 		qtype,ip,qname string
-	}
-	type R struct {
-		rcode,ip,qname string
 	}
 
 	fields := make(map[string]int)
 	sources := make(map[string]int)
 	domains := make(map[string]int)
-	info := make(map[Q]int)
-	responses := make(map[R]int)
+	info := make(map[Key]int)
+	responses := make(map[int]int)
 
 	if len(batch) == 0 {
 		return nil
 	}
 	fields["TOTALQ"] = 0
 	fields["TOTALR"] = 0
-	fields["NOERROR"] = 0
-	fields["NXDOMAIN"] = 0
 
 	now :=  time.Now()
 
 	for _,b := range batch {
-		src := b.SrcIP.String()
-		dst := b.DstIP.String()
-
+		ip := b.SrcIP.String()
 		if b.DNS.Response  {
-			fields["TOTALR"]++
-			rc := dns.RcodeToString[b.DNS.Rcode]
-			if b.DNS.Rcode == 0 {fields["NOERROR"]++}
-			if b.DNS.Rcode == 3 {fields["NXDOMAIN"]++}
-			name := ""
-			if len(b.DNS.Question) > 0 {name=strings.ToLower(b.DNS.Question[0].Name)}
-			responses[R{rc,dst,name}] = 1 + responses[R{rc,dst,name}]
+			fields["TOTALR"] = 1 + fields["TOTALR"]
+			responses[b.DNS.Rcode] = 1 + responses[b.DNS.Rcode]
 		} else {
 			fields["TOTALQ"] = 1 + fields["TOTALQ"]
-			sources[src] = 1 + sources[src]
+			sources[ip] = 1 + sources[ip]
 			for _,d := range b.DNS.Question {
 				name := strings.ToLower(d.Name)
 				domains[name] = 1 + domains[name]
 				qt := dns.TypeToString[d.Qtype]
 				fields[qt] = 1 + fields[qt]
-				info[Q{qt,src,name}] = 1 + info[Q{qt,src,name}]
+				info[Key{qt,ip,name}] = 1 + info[Key{qt,ip,name}]
 			}
 		}
 	}
 
 	// Adding some stats
+	fields["NOERROR"] = responses[0]
+	fields["NXDOMAIN"] = responses[3]
 	fields["UNIQUERY"] = len(domains)
 
   // Store TNSM stats 
@@ -93,14 +81,22 @@ func InfluxAggAndStore(writeAPI api.WriteAPI, batch []cdns.DNSResult) error {
 		}
 	}()
 
-	// Store also responses
-	  go func() {
-		for k,v := range responses {
-			p := influxdb2.NewPoint("responses",
-				map[string]string{
-					"rcode" : k.rcode,
-					"ip" : k.ip,
-					"qname" : k.qname},
+	// Store also sources
+  go func() {
+		for k,v := range sources {
+			p := influxdb2.NewPoint("source",
+				map[string]string{"ip" : k},
+				map[string]interface{}{"freq" : v},
+                                now)
+			writeAPI.WritePoint(p)
+		}
+	}()
+
+	// Store domain names
+	go func() {
+		for k,v := range domains {
+			p := influxdb2.NewPoint("domain",
+				map[string]string{"qname" : k},
 				map[string]interface{}{"freq" : v},
                                 now)
 			writeAPI.WritePoint(p)
