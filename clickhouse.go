3c8b81034325679b7c5b7b6ab0256ffff7d5d5aa
Javier Bustos <jbustos@niclabs.cl>
2021-03-30 15:14:19 -0300

    fix typo from typo

--- clickhouse.go
+++ clickhouse.go
@@ -93,7 +93,7 @@ func SendData(connect clickhouse.Clickhouse, batch []cdns.DNSResult, server []by
 		return err
 	}
 
-	_, err = connect.Prepare("INSERT INTO DNS_LOG (DnsDate, timestamp, Server, IPVersion, IPPrefix, Protocol, QR, OpCode, Class, Type, ResponcsCode, Question, Size, Edns0Present, DoBit) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+	_, err = connect.Prepare("INSERT INTO DNS_LOG (DnsDate, timestamp, Server, IPVersion, IPPrefix, Protocol, QR, OpCode, Class, Type, ResponseCode, Question, Size, Edns0Present, DoBit) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
 	if err != nil {
 		return err
 	}
